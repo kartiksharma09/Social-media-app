e89cb32fbbb671bcc5ae2c93464cdf978202bfe4
kartik sharma <kartik19@navgurukul.org>
2021-05-11 14:31:54 +0530

    folder 4 done

--- social-media-app/config/db.js
+++ social-media-app/config/db.js
@@ -5,7 +5,7 @@ const db = config.get('mongoURI');
 const connectDB = async() => {
     try{
         await mongoose.connect(db,{
-            useNewUrlParser:true,useUnifiedTopology: true,useCreateIndex:true
+            useNewUrlParser:true , useUnifiedTopology:true , useCreateIndex:true , useFindAndModify:false
         });
         console.log('mongoDB connected');
     } catch(err){
