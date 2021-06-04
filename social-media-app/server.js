d92c9a4e35f9b0dcf0eca8e8caa06f41a513e485
kartik sharma <kartik19@navgurukul.org>
2021-06-04 11:32:36 +0530

    Prepared for deployment

--- social-media-app/server.js
+++ social-media-app/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 
@@ -10,7 +11,6 @@ connectDB();
 // Init middleware
 app.use(cors())
 app.use(express.json({ extended: false }));
-app.get("/", (req, res) => res.send("API Running"));
 
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
@@ -18,6 +18,19 @@ app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/auth", require("./routes/api/auth"));
 
-const PORT = 6000;
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('my-app/build'));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'my-app', 'build', 'index.html'))
+    });
+}
+
+
+
+
+const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
