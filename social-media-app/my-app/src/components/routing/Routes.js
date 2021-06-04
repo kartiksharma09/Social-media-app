0fbb59cd88619873d6fea206a0ded2e4bcb525f1
kartik sharma <kartik19@navgurukul.org>
2021-06-04 11:57:40 +0530

    bug fixed

---
+++ social-media-app/my-app/src/components/routing/Routes.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {Route, Switch} from 'react-router-dom';
+import Login from '../auth/Login';
+import Alert from '../layout/Alert';
+import Register from '../auth/Register'; 
+import Dashboard from '../dashboard/Dashboard'; 
+import CreateProfile from '../profile-forms/CreateProfile';
+import AddExperience from '../profile-forms/AddExperience';
+import AddEducation from '../profile-forms/AddEducation';
+import EditProfile from '../profile-forms/EditProfile'; 
+import PrivateRoute from '../routing/PrivateRoute'; 
+import Profiles from '../profiles/Profiles';
+import Posts from '../posts/Posts';
+import Post from '../post/Post';
+import NotFound from '../layout/NotFound';
+import Profile from '../profile/Profile';
+
+export const Routes = () => {
+    return (
+        <section className="container">
+        <Alert />
+        <Switch>
+          <Route exact path="/register" component={Register}/>
+          <Route exact path="/login" component={Login}/>
+          <Route exact path="/profiles" component={Profiles}/>
+          <Route exact path="/profile/:id" component={Profile}/>
+          <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+          <PrivateRoute exact path="/create-profile" component={CreateProfile}/>
+          <PrivateRoute exact path="/edit-profile" component={EditProfile}/>
+          <PrivateRoute exact path="/add-experience" component={AddExperience}/>
+          <PrivateRoute exact path="/add-education" component={AddEducation}/>
+          <PrivateRoute exact path="/posts" component={Posts}/>
+          <PrivateRoute exact path="/posts/:id" component={Post}/>
+          <Route component={NotFound}/>
+
+
+        </Switch>
+      </section>
+    )
+}
